8ac37a0887a70771ff6aa565dbd02371f3ce0c4b
jeanpi.jpct@gmail.com <jeanpi.jpct@gmail.com>
2025-02-18 15:50:34 +0000

    actualizando servidor

--- database/migrations/2025_02_10_114100_update_history_material_table.php
+++ database/migrations/2025_02_10_114100_update_history_material_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('raw_materials_history', function (Blueprint $table) {
-            $table->bigInteger('raw_provider')->unsigned()->after('product_id');
+            $table->bigInteger('raw_provider')->unsigned()->nullable()->after('product_id');
             $table->foreign('raw_provider')->references('id')->on('provider');
 
         });
