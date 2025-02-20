f17f96665fa171b7efc6dab9e4515f5470aac8ae
Jean Pier Carrasco Tamariz <jeanpi.jpct@gmail.com>
2025-02-20 11:17:33 +0100

    cambiando la subconsulta por funcion

--- app/Models/Quotation.php
+++ app/Models/Quotation.php
@@ -41,62 +41,59 @@ class Quotation extends Model
     ];
     public function products()
     {
-        return $this->belongsToMany(Products::class,'quotations_details','quotation_id','product_id')
-        ->withPivot('detail_quantity','detail_price_unit','detail_price_buy','detail_price_additional','detail_total','detail_status','quotation_description')
-        ->withTimestamps();
+        return $this->belongsToMany(Products::class, 'quotations_details', 'quotation_id', 'product_id')
+            ->withPivot('detail_quantity', 'detail_price_unit', 'detail_price_buy', 'detail_price_additional', 'detail_total', 'detail_status', 'quotation_description')
+            ->withTimestamps();
     }
-    public static function getQuotations($search,$filters = []) {
-        $query = Quotation::select("quotations.id","quotation_code","quotation_total","quotation_type_money","quotation_status","customer_name")
-        ->selectRaw("DATE_FORMAT(quotation_date_issue,'%d/%m/%Y') AS date_issue,CONCAT(user_name,' ',user_last_name) AS name_quoter,(
-        SELECT sub_categorie_name FROM (SELECT sub_categorie_name,SUM(detail_total) AS detail_quotation FROM quotations_details 
-        INNER JOIN products ON products.id = product_id 
-        INNER JOIN sub_categories ON sub_categories.id = products.sub_categorie 
-        WHERE quotation_id = quotations.id GROUP BY sub_categories.id
-        ) AS table_temp ORDER BY detail_quotation DESC LIMIT 1 ) AS sub_categorie_name")
-        ->join('customers','quotation_customer','=','customers.id')
-        ->join('users','quotation_quoter','=','users.id')
-        ->where(function($query)use($search){
-            $query->where('customer_name','LIKE','%'.$search.'%')
-            ->orWhereRaw("CONCAT(user_name,' ',user_last_name) LIKE CONCAT('%',?,'%') OR quotation_code LIKE CONCAT('%',?,'%')",[$search,$search])
-            ->orWhereRaw("(SELECT sub_categorie_name FROM quotations_details INNER JOIN products ON products.id = product_id INNER JOIN sub_categories ON sub_categories.id = products.sub_categorie WHERE quotation_id = quotations.id ORDER BY detail_total DESC LIMIT 1) LIKE CONCAT('%',?,'%')",[$search]);
-
-        });
+    public static function getQuotations($search, $filters = [])
+    {
+        $query = Quotation::select("quotations.id", "quotation_code", "quotation_total", "quotation_type_money", "quotation_status", "customer_name")
+            ->selectRaw("DATE_FORMAT(quotation_date_issue,'%d/%m/%Y') AS date_issue,CONCAT(user_name,' ',user_last_name) AS name_quoter,fn_name_subcategory_quotation(quotations.id) AS sub_categorie_name")
+            ->join('customers', 'quotation_customer', '=', 'customers.id')
+            ->join('users', 'quotation_quoter', '=', 'users.id')
+            ->where(function ($query) use ($search) {
+                $query->where('customer_name', 'LIKE', '%' . $search . '%')
+                    ->orWhereRaw("CONCAT(user_name,' ',user_last_name) LIKE CONCAT('%',?,'%') OR quotation_code LIKE CONCAT('%',?,'%')", [$search, $search])
+                    ->orWhereRaw("fn_name_subcategory_quotation(quotations.id) LIKE CONCAT('%',?,'%')", [$search]);
+            });
         foreach ($filters as $filter) {
-            $query = $query->where($filter['column'],$filter['sign'],$filter['value']);
+            $query = $query->where($filter['column'], $filter['sign'], $filter['value']);
         }
         return $query;
     }
     public function customer()
     {
-        return $this->belongsTo(Customers::class,'quotation_customer');
+        return $this->belongsTo(Customers::class, 'quotation_customer');
     }
     public function user()
     {
-        return $this->belongsTo(User::class,'quotation_quoter');
+        return $this->belongsTo(User::class, 'quotation_quoter');
     }
     public function contact()
     {
-        return $this->belongsTo(Contacts::class,'quotation_customer_contact');
+        return $this->belongsTo(Contacts::class, 'quotation_customer_contact');
     }
-    public static function getQuotationsForOrders($typeMoney,$includeIgv,$customer) {
-        return Quotation::select('quotations.id','quotation_total','quotation_code','quotation_project','contact_name','contact_email','contact_number')
-        ->selectRaw("DATE_FORMAT(quotation_date_issue,'%d/%m/%Y') AS date_issue, 1 AS checked")
-        ->leftJoin('contacts','quotation_customer_contact','=','contacts.id')
-        ->where(['quotation_status' => 1, 'quotation_type_money' => $typeMoney,'quotation_include_igv' => $includeIgv, 'quotation_customer' => $customer])->whereNull('order_id')->get();
+    public static function getQuotationsForOrders($typeMoney, $includeIgv, $customer)
+    {
+        return Quotation::select('quotations.id', 'quotation_total', 'quotation_code', 'quotation_project', 'contact_name', 'contact_email', 'contact_number')
+            ->selectRaw("DATE_FORMAT(quotation_date_issue,'%d/%m/%Y') AS date_issue, 1 AS checked")
+            ->leftJoin('contacts', 'quotation_customer_contact', '=', 'contacts.id')
+            ->where(['quotation_status' => 1, 'quotation_type_money' => $typeMoney, 'quotation_include_igv' => $includeIgv, 'quotation_customer' => $customer])->whereNull('order_id')->get();
     }
-    public static function getQuotationsReport($startDate,$finalDate,$order = '0') {
-        $quotationsQuery = Quotation::select("quotations.id","quotation_code","order_code","quotation_date_issue","customer_name","user_name","user_last_name","contrie","departament_name","quotation_status","quotation_type_money","quotation_change_money")
-        ->selectRaw("DATE_FORMAT(orders.created_at, '%Y-%m-%d') AS order_create")
-        ->join('users','users.id','=','quotation_quoter')
-        ->join('customers','customers.id','=','quotation_customer')
-        ->join('contries','contries.id','=','customer_contrie')
-        ->leftJoin('districts','districts.id','=','customer_district')
-        ->leftJoin('departaments','departaments.id','=','district_departament')
-        ->leftJoin('orders','order_id','=','orders.id')
-        ->whereBetween('quotation_date_issue',[$startDate,$finalDate]);
-        if($order === '1') {
+    public static function getQuotationsReport($startDate, $finalDate, $order = '0')
+    {
+        $quotationsQuery = Quotation::select("quotations.id", "quotation_code", "order_code", "quotation_date_issue", "customer_name", "user_name", "user_last_name", "contrie", "departament_name", "quotation_status", "quotation_type_money", "quotation_change_money")
+            ->selectRaw("DATE_FORMAT(orders.created_at, '%Y-%m-%d') AS order_create")
+            ->join('users', 'users.id', '=', 'quotation_quoter')
+            ->join('customers', 'customers.id', '=', 'quotation_customer')
+            ->join('contries', 'contries.id', '=', 'customer_contrie')
+            ->leftJoin('districts', 'districts.id', '=', 'customer_district')
+            ->leftJoin('departaments', 'departaments.id', '=', 'district_departament')
+            ->leftJoin('orders', 'order_id', '=', 'orders.id')
+            ->whereBetween('quotation_date_issue', [$startDate, $finalDate]);
+        if ($order === '1') {
             return $quotationsQuery->whereNotNull('order_id');
-        }else if($order === '2'){
+        } else if ($order === '2') {
             return $quotationsQuery->whereNull('order_id');
         }
         return $quotationsQuery;
